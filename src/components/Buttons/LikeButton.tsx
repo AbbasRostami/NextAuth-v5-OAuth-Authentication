621c03283a89d43492f9e285c0aa7a3b59e24c8d
AbbasRostami <abbas.cracker@gmail.com>
2025-03-29 17:46:32 +0330

    Add LikeButton component for liking posts with optimistic UI updates

---
+++ src/components/Buttons/LikeButton.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useTransition, useState } from "react";
+import { likePost } from "@/actions/postActions";
+
+export default function LikeButton({
+  postId,
+  initialLikes,
+}: {
+  postId: number;
+  initialLikes: number;
+}) {
+  const [likes, setLikes] = useState(initialLikes);
+  const [isPending, startTransition] = useTransition();
+
+  const handleLike = () => {
+    startTransition(async () => {
+      const result = await likePost(postId);
+      setLikes(result.likes);
+    });
+  };
+  
+
+  return (
+    <button
+      onClick={handleLike}
+      disabled={isPending}
+      className={`mt-3 w-full text-center bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition 
+        ${
+        isPending && "opacity-50 cursor-not-allowed"
+      }`}
+    >
+      {isPending ? "Liking..." : `👍 Like (${likes})`}
+    </button>
+  );
+}
